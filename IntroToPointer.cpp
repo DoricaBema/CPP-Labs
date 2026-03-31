310851859f0b59d74e1887e10c8af75a8b80e365
DoricaBema <bsc-com-ne-32-24@unima.ac.mw>
2026-03-31 12:00:39 -0700

    introtopointer

---
+++ IntroToPointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //declare pointer and initialize it
+    int* pPointer = nullptr;
+
+    int integerVar = 5;
+
+    pPointer = &integerVar;
+
+    cout << "integerVar: " << integerVar << endl;
+
+    cout << "Address of integerVar: " << &integerVar << endl;
+
+    cout << "pPointer: " << pPointer << endl;
+
+    cout << "Address of pPointer" << &pPointer << endl;
+
+    return 0;
+}
